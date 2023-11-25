d81f2ba7fc9009a73d86cd4b1e40bb40393ddc73
Arun <arun@technative.in>
2023-11-26 01:42:27 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+let greet = ""
+let cssStyle = {}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+let currTime = new Date();
+currTime = currTime.getHours()
+
+if(currTime >= 1 && currTime < 12){
+  greet = 'Good Morning'
+  cssStyle.color = 'green'
+}else if(currTime >= 12 && currTime < 19){
+  greet = 'Good Afternoon'
+  cssStyle.color = 'orange'
+}else{
+  greet = 'Good Night'
+  cssStyle.color = 'black'
+}
+
+
+
+ReactDOM.render(
+  <>
+  <div>
+  <h1>Hello sir, <span style={cssStyle}>{greet}</span></h1>
+  </div>
+  </>,
+  document.getElementById('root')
+)
